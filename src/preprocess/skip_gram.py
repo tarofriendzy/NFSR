929e79c9b7a6ff87175533ce762c7947b3205ef2
tarofriendzy <tarofriendzy@gmail.com>
2023-09-03 20:49:22 +0800

    Update skip_gram.py

--- src/preprocess/skip_gram.py
+++ src/preprocess/skip_gram.py
@@ -1,6 +1,4 @@
-"""
-Word2vec模型
-"""
+
 import io
 import tensorflow as tf
 from tensorflow.keras import Model, optimizers
@@ -8,16 +6,9 @@ from tensorflow.keras.layers import Dot, Embedding, Flatten
 
 
 class Word2Vec(Model):
-    """
-    Word2Vec的Skip Gram
-    """
+
     def __init__(self, vocab_size, embedding_dim, num_ns):
-        """
-        初始化
-        :param vocab_size: 词汇量,即：最大整数索引 + 1
-        :param embedding_dim: 密集嵌入的维度
-        :param num_ns: 每个正上下文词的负采样数量
-        """
+
         super(Word2Vec, self).__init__()
         self.target_embedding = Embedding(input_dim=vocab_size,
                                           output_dim=embedding_dim,
@@ -41,15 +32,7 @@ class Word2Vec(Model):
 
 
 def train_model(dataset, vocab_size, num_ns, embedding_dim, num_epochs, learning_rate):
-    """
-    训练
-    :param learning_rate:
-    :param dataset: 数据集
-    :param vocab_size: 词汇量的大小
-    :param num_epochs: 训练的轮数
-    :param embedding_dim: 嵌入的维度
-    :return:
-    """
+
     word2vec = Word2Vec(vocab_size=vocab_size, embedding_dim=embedding_dim, num_ns=num_ns)
     word2vec.compile(optimizer=optimizers.SGD(learning_rate=learning_rate),
                      loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
